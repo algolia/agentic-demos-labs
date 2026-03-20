d50a1648ab119bf6ec7bb55ce802c1906d9fdb11
Youcef Mammar <youcef.mammar@algolia.com>
2026-03-20 15:02:24 +0100

    added codesandbox specifics

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['*.csb.app'],
   reactStrictMode: true,
   experimental: {
     viewTransition: true,
